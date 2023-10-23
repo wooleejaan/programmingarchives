caa3b6277ab13130a5228c39318c08b273d54a81
wooleejaan <wooleejaan@gmail.com>
2023-10-23 15:57:29 +0900

    feat: home 페이지 내 api 통신 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,12 @@
+import { useEffect } from "react";
+
+import getHttpForTest from "@r/services/api/fetch/getHttpForTest";
+import wrapperForFetch from "@r/services/api/fetch/wrapperForFetch";
+
 export default function Home() {
+  useEffect(() => {
+    wrapperForFetch(getHttpForTest, console.log);
+  }, []);
+
   return <div>home</div>;
 }
